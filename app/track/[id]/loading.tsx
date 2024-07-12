d2ccfd39889f8ef2dfd555c3d29644284900b581
Camilo Rodriguez <aeroxmotion@hotmail.com>
2024-07-12 10:45:47 -0500

    feat: add loading partial track

---
+++ app/track/[id]/loading.tsx
@@ -0,0 +1,21 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { GoBackButton } from "@/app/_components/GoBackButton";
+
+export default function TrackDetailLoading() {
+  return (
+    <div className="pt-16">
+      <GoBackButton />
+
+      <main className="flex flex-row mb-8">
+        <Skeleton style={{ width: 300, height: 300 }} />
+
+        <div className="pl-8">
+          <Skeleton className="w-64 h-12 mb-2" />
+          <Skeleton className="w-32 h-6 mb-4" />
+          <Skeleton className="w-48 h-6 mb-4" />
+          <Skeleton className="w-32 h-12 mb-4" />
+        </div>
+      </main>
+    </div>
+  );
+}
